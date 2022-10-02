ea431324fa85c3623fe91f9117ae701de0188e9f
Daniil <daniil9360451@gmail.com>
2022-10-02 17:17:20 +0300

    Add project files.

---
+++ BookShop.WEB/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.WEB.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
